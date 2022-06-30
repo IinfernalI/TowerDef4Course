9340c6d375f033a434a6499792d7e46dac53f050
Arthur_Lebedev <IinfernalI21@gmail.com>
2022-06-30 18:21:24 +0300

    Created_radius_to_enemy, Created_fire_radius, Created_the_fire_method, Created_method_of_checking_radius, Created_check_for_enemy

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -8,8 +8,34 @@ public class Tower : MonoBehaviour
 
     [SerializeField] private Transform targetEnemy;
 
+    [SerializeField] private float shootRange;
+
+    [SerializeField] private ParticleSystem bulletParticle;
+
     void Update()
     {
         towerTop.LookAt(targetEnemy);
+        if (targetEnemy) { Fire(); }
+        else { Shoot(false); }
+    }
+    
+    private void Fire()
+    {
+        float distanceToEnemy = Vector3.Distance(targetEnemy.position, transform.position);
+        if (distanceToEnemy <= shootRange)
+        {
+            Shoot(true);
+        }
+        else
+        {
+            Shoot(false);
+        }
+    }
+
+    private void Shoot(bool canShoot)
+    {
+        //bulletParticle.enableEmission = canShoot;   Метод устарел
+        var emission = bulletParticle.emission;
+        emission.enabled = canShoot;
     }
 }
